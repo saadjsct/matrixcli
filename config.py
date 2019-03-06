09e8b4782d36e47cb5b079734b9b1122c4575036
ahmed saad <saad@saadnpq.com>
2019-03-06 02:23:24 +0200

    sample config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+# def account1_password_eval():
+#     # you can substitute your password directly here
+#     # or you can evaluate the password in any way you want if you don't
+#     # want it to appear in plain text
+#     return "account1secretpassword"
+
+# def account2_matrixorg_password_eval():
+#     return "account2secretpassword"
+
+# def account3_password_eval_any_name():
+#     return "yXkdsjhslkjhdlksjhffffffkkssskjsdhkljhssdc"
+
+# accounts=[
+
+#         { "server":"https://example.com",
+#           "username":"account1",
+#           "passeval":account1_password_eval },
+
+#         { "server":"https://matrix.org",
+#           "username":"account2",
+#           "passeval":account2_matrixorg_password_eval},
+
+#         { "server":"https://example.com",
+#           "username":"account3",
+#           "passeval":account3_password_eval_any_name },
+# ]
+
