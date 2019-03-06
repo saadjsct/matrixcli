69a230d39093c82d34279f055526eedbcbeb67d4
ahmed saad <saad@saadnpq.com>
2019-03-06 02:22:06 +0200

    added LICENSE and some comments

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
+#!/bin/python3
+
 from setuptools import setup, find_packages
+import os
+
+HOME = os.getenv('HOME')
 
 setup(
     name='matrixcli',
     version='0.1',
     description='command line matrix client',
-    long_description='README.md',
-    long_description_content_type="text/markdown",
+    long_description='for more info https://git.saadnpq.com/saad/matrixcli',
+    long_description_content_type="text/plain",
     author='saadnpq',
     author_email='saad@saadnpq.com',
-    url='',
-    packages=find_packages(),
+    url='https://git.saadnpq.com/saad/matrixcli',
     license='Apache License, Version 2.0',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Communications :: Chat',
         'Topic :: Communications :: Conferencing',
+        'Operating System :: POSIX :: Linux',
+ 
     ],
-    keywords='chat matrix matrix.org command line',
-    install_requires=[
-        'requests',
-        'urllib3',
-    ],
+    keywords='chat matrix command line',
+    install_requires=['matrix_client',],
+    data_files=[(HOME+'/.config/matrixcli', ['config.py'])],
+    scripts=['matrixcli']
 )
