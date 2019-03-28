0b1a8ab7a28961ed87c0e5cc1057516f7e001173
ahmed saad <saad@saadnpq.com>
2019-03-28 17:50:08 +0200

    add a systemd unit file and changed to system wide installation

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 from setuptools import setup, find_packages
-import os
-
-HOME = os.getenv('HOME')
 
 setup(
     name='matrixcli',
@@ -14,17 +10,15 @@ setup(
     author='saadnpq',
     author_email='saad@saadnpq.com',
     url='https://git.saadnpq.com/saad/matrixcli',
-    license='Apache License, Version 2.0',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Communications :: Chat',
         'Topic :: Communications :: Conferencing',
         'Operating System :: POSIX :: Linux',
- 
     ],
     keywords='chat matrix command line',
     install_requires=['matrix_client',],
-    data_files=[(HOME+'/.config/matrixcli', ['config.py'])],
+    data_files=[('/etc/matrixcli', ['config.py']),('/lib/systemd/user', ['matrixcli.service'])],
     scripts=['matrixcli']
 )
